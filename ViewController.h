d8977b10b114c1f280b9415046f374e71823378f
sMike <bp34@163.com>
2015-05-04 11:30:35 +0800

    自定 iVersion 版本检查提示框
    自定 iVersion
    版本检查提示框，包括区域（中国），有最新版本，检查版本错误（如未上架，未匹配到等），已经是最新版本的几种提示，去掉忽略按钮。

---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iVersionDemo
+//
+//  Created by LiHaozhen on 15/5/4.
+//  Copyright (c) 2015年 LiHaozhen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UITableViewController
+
+
+@end
+
---
+++ ViewController.m
@@ -0,0 +1,46 @@
+//
+//  ViewController.m
+//  iVersionDemo
+//
+//  Created by LiHaozhen on 15/5/4.
+//  Copyright (c) 2015年 LiHaozhen. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "iVersion.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [[iVersion sharedInstance] setPreviewMode:YES];
+    [[iVersion sharedInstance] checkForNewVersion];
+    
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+- (IBAction)wxSwitchValueChanged:(UISwitch *)sender {
+    
+    if (sender.isOn) {
+        
+        [iVersion sharedInstance].applicationBundleID = @"com.tencent.xin";
+    }else{
+        
+        [iVersion sharedInstance].applicationBundleID = nil;
+    }
+}
+@end
